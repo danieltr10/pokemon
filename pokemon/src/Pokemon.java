872464d6c9ebd7493c791c91ea269865d3e3e90e
Daniel Rizzuto <danielrizzuto@icloud.com>
2015-04-28 16:56:12 -0300

    Fiz o construtor de Pokemon

--- pokemon/src/Pokemon.java
+++ pokemon/src/Pokemon.java
@@ -3,17 +3,31 @@ public class Pokemon {
 	
 	private String nome;
 	private int HP;
-	private Ataque[] listaDeAtaques = new Ataque[3];
-	private String tipo;
+	private Ataque[] listaDeAtaques;
+	//private String tipo;
 	
-	public Pokemon (String nomePokemon, String tipo, int HP, String nomeAtaque1, int prioridadeAtaque1, String nomeAtaque2, int prioridadeAtaque2, String nomeAtaque3, int prioridadeAtaque3, String nomeAtaque4, int prioridadeAtaque4) {
+	public Pokemon (String nomePokemon, String tipo, int HP, String nomeAtaque1, int danoAtaque1, int prioridadeAtaque1, String nomeAtaque2, int danoAtaque2, int prioridadeAtaque2, String nomeAtaque3, int danoAtaque3, int prioridadeAtaque3, String nomeAtaque4, int danoAtaque4, int prioridadeAtaque4) {
 		
 		nome = nomePokemon;
 		this.HP = HP;
-		listaDeAtaques[0].nomeAtaque = nomeAtaque1;
+		
+		listaDeAtaques[0] = new Ataque(prioridadeAtaque1);
+		listaDeAtaques[1] = new Ataque(prioridadeAtaque2);
+		listaDeAtaques[2] = new Ataque(prioridadeAtaque3);
+		listaDeAtaques[3] = new Ataque(prioridadeAtaque4);
+		
+		listaDeAtaques[0].setNomeAtaque(nomeAtaque1);
+		listaDeAtaques[1].setNomeAtaque(nomeAtaque2);
+		listaDeAtaques[2].setNomeAtaque(nomeAtaque3);
+		listaDeAtaques[3].setNomeAtaque(nomeAtaque4);
+		
+		listaDeAtaques[0].setDano(danoAtaque1);
+		listaDeAtaques[1].setDano(danoAtaque2);
+		listaDeAtaques[2].setDano(danoAtaque3);
+		listaDeAtaques[3].setDano(danoAtaque4);
+		
 		
 	}
-	
 
 
 }
